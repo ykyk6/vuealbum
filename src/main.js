96b6ac4c2e957f3b7c194169200bb0d265d9bd5f
yk6yk6 <yknoch7@gmail.com>
2020-12-14 11:05:45 +0800

    111111112222

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import 'sweetalert2/dist/sweetalert2.min.css'
 import ImgInputer from 'vue-img-inputer'
 import 'vue-img-inputer/dist/index.css'
 import Photoswipe from 'vue-pswipe'
-import VueAnalytics from 'vue-analytics'
+import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -19,8 +19,10 @@ Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
 Vue.use(VueSweetalert2)
 Vue.use(Photoswipe)
-Vue.use(VueAnalytics, {
-id: process.env.VUE_APP_GA
+Vue.use(VueGtag, {
+  config: {
+    id: process.env.VUE_APP_GA
+  }
 })
 Vue.component('ImgInputer', ImgInputer)
 
